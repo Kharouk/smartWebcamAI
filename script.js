ac879f93648f9adccfd621d0eaa8e4b7e3afc173
Alex Kharouk <akharoukpp@gmail.com>
2021-06-12 20:49:58 +0100

    can detect humans

--- script.js
+++ script.js
@@ -1,2 +1,98 @@
 // tf
-console.log(tf);
+console.log(cocoSsd);
+
+const video = document.getElementById("webcam");
+const liveView = document.getElementById("liveView");
+const mainSection = document.getElementById("main");
+const enableWebcamButton = document.getElementById("webcamButton");
+
+const getUserMediaSupported = () => !!navigator?.mediaDevices.getUserMedia;
+
+let children = [];
+
+function predictWebcam() {
+  // Now let's start classifying a frame in the stream.
+  model.detect(video).then(function (predictions) {
+    // Remove any highlighting we did previous frame.
+    for (let i = 0; i < children.length; i++) {
+      liveView.removeChild(children[i]);
+    }
+    children.splice(0);
+    console.log(predictions);
+    // Now lets loop through predictions and draw them to the live view if
+    // they have a high confidence score.
+    for (let n = 0; n < predictions.length; n++) {
+      // If we are over 66% sure we are sure we classified it right, draw it!
+      if (predictions[n].score > 0.66) {
+        const p = document.createElement("p");
+        p.innerText =
+          predictions[n].class +
+          " - with " +
+          Math.round(parseFloat(predictions[n].score) * 100) +
+          "% confidence.";
+        p.style =
+          "margin-left: " +
+          predictions[n].bbox[0] +
+          "px; margin-top: " +
+          (predictions[n].bbox[1] - 10) +
+          "px; width: " +
+          (predictions[n].bbox[2] - 10) +
+          "px; top: 0; left: 0;";
+
+        const highlighter = document.createElement("div");
+        highlighter.setAttribute("class", "highlighter");
+        highlighter.style =
+          "left: " +
+          predictions[n].bbox[0] +
+          "px; top: " +
+          predictions[n].bbox[1] +
+          "px; width: " +
+          predictions[n].bbox[2] +
+          "px; height: " +
+          predictions[n].bbox[3] +
+          "px;";
+
+        liveView.appendChild(highlighter);
+        liveView.appendChild(p);
+        children.push(highlighter);
+        children.push(p);
+      }
+    }
+
+    // Call this function again to keep predicting when the browser is ready.
+    window.requestAnimationFrame(predictWebcam);
+  });
+}
+
+let model = undefined;
+
+cocoSsd.load().then((loadedModel) => {
+  model = loadedModel;
+  console.log(model);
+  mainSection.classList.remove("hidden");
+});
+
+function enableCam(event) {
+  if (!model) {
+    return;
+  }
+
+  // Hide the button once clicked.
+  event.target.classList.add("removed");
+
+  // do not enable audio:
+  const constraints = {
+    video: true,
+  };
+
+  navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+    video.srcObject = stream;
+    video.addEventListener("loadeddata", predictWebcam);
+  });
+}
+
+if (getUserMediaSupported()) {
+  enableWebcamButton.addEventListener("click", enableCam);
+} else {
+  alert("Webcam Permission is not supported by your browser");
+}
